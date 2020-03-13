a552bd62d576ef87afc9ffb2250e0cc10d7a8858
Abhijeet Khire <abhijeet.khire@Eternus.local>
2020-03-13 20:20:59 +0530

    basic graphql

---
+++ src/queries/queries.js
@@ -0,0 +1,59 @@
+import { gql } from 'apollo-boost';
+
+const getAuthorsQuery = gql`
+    {
+        authors {
+            name
+            id
+        }
+    }
+`;
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`;
+
+const addBookMutation = gql`
+    mutation AddBook($name: String!, $genre: String!, $authorId: ID!){
+        addBook(name: $name, genre: $genre, authorId: $authorId){
+            name
+            id
+        }
+    }
+`;
+
+const addMyBookMutation = gql`
+    mutation RemoveBook($id: ID!){
+        removeBook(id: $id){
+            id
+        }
+    }
+`;
+
+
+
+const getBookQuery = gql`
+    query GetBook($id: ID){
+        book(id: $id) {
+            id
+            name
+            genre
+            author {
+                id
+                name
+                age
+                books {
+                    name
+                    id
+                }
+            }
+        }
+    }
+`;
+
+export { getAuthorsQuery, getBooksQuery, addBookMutation, addMyBookMutation , getBookQuery };
